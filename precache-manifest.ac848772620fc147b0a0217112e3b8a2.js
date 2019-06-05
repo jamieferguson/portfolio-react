d67150a728361451f4286bca96fe5c92a25bba6a
jamieferguson <jamie@shorthand.com>
2019-06-05 18:11:49 +1000

    Updates

---
+++ precache-manifest.ac848772620fc147b0a0217112e3b8a2.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaf9ad07f1d0fe0693c796fe3930bbd",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "15f6bc00e27e006670e0",
+    "url": "/portfolio-react/static/css/main.0e806e85.chunk.css"
+  },
+  {
+    "revision": "01935f872ea39157d39a",
+    "url": "/portfolio-react/static/js/2.8e61a224.chunk.js"
+  },
+  {
+    "revision": "15f6bc00e27e006670e0",
+    "url": "/portfolio-react/static/js/main.28cdfd99.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "60641adfcf3694f864c86896c05d67c4",
+    "url": "/portfolio-react/static/media/index-aus.60641adf.jpg"
+  },
+  {
+    "revision": "2adc84a87b6e71b62049e5ccb8dd0138",
+    "url": "/portfolio-react/static/media/index-shorthand.2adc84a8.jpg"
+  },
+  {
+    "revision": "68aab70cb62b21009e2fa5f346fc6956",
+    "url": "/portfolio-react/static/media/index-tgm.68aab70c.jpg"
+  },
+  {
+    "revision": "923dd834e8d616b9a9df1440bf428f87",
+    "url": "/portfolio-react/static/media/index-viator.923dd834.jpg"
+  },
+  {
+    "revision": "c28d1b135f25817a2c2759c6c77ffef7",
+    "url": "/portfolio-react/static/media/story-442-brazil.c28d1b13.jpg"
+  },
+  {
+    "revision": "f9c2429c6438203582c25484aaa7d23b",
+    "url": "/portfolio-react/static/media/story-agnsw.f9c2429c.jpg"
+  },
+  {
+    "revision": "0cbe9e5908c3c55177b51d05a453115f",
+    "url": "/portfolio-react/static/media/story-aus.0cbe9e59.jpg"
+  },
+  {
+    "revision": "f8ad6132a962846470e63b39d324f7da",
+    "url": "/portfolio-react/static/media/story-espn.f8ad6132.jpg"
+  },
+  {
+    "revision": "5a37f76ac81a7385b127a25636e730d0",
+    "url": "/portfolio-react/static/media/story-mv.5a37f76a.jpg"
+  },
+  {
+    "revision": "8e37041b081361525956463d781121ac",
+    "url": "/portfolio-react/static/media/story-vivid.8e37041b.jpg"
+  },
+  {
+    "revision": "e8050849033d44c105861edb6f23313d",
+    "url": "/portfolio-react/static/media/work-shorthand-01.e8050849.jpg"
+  },
+  {
+    "revision": "d5de646743439aff74e0e06f42009785",
+    "url": "/portfolio-react/static/media/work-tgm-alz.d5de6467.jpg"
+  },
+  {
+    "revision": "b6203c4b584f45c38bb9dfbc1e0e8aa8",
+    "url": "/portfolio-react/static/media/work-tgm-bulga.b6203c4b.jpg"
+  },
+  {
+    "revision": "60ded6b1df70bd5675db72bed4c8baef",
+    "url": "/portfolio-react/static/media/work-tgm-pharma.60ded6b1.jpg"
+  },
+  {
+    "revision": "e656cf280720d55736f8d58454afaca4",
+    "url": "/portfolio-react/static/media/work-tgm-rockart.e656cf28.jpg"
+  },
+  {
+    "revision": "284e867a463bc27627e85facdb39b7ac",
+    "url": "/portfolio-react/static/media/work-tgm-wire-01.284e867a.jpg"
+  },
+  {
+    "revision": "7c9e73da5d13051ffbccb44df8500107",
+    "url": "/portfolio-react/static/media/work-tgm-wire-02.7c9e73da.jpg"
+  },
+  {
+    "revision": "f000cf81a1ac9e231670111353acaa59",
+    "url": "/portfolio-react/static/media/work-tgm-wire-03.f000cf81.jpg"
+  },
+  {
+    "revision": "293c5a978149b28e3f3a191cab7c0c6e",
+    "url": "/portfolio-react/static/media/work-tgm-wire-04.293c5a97.jpg"
+  },
+  {
+    "revision": "ee88173a021581bc25db6b739dacca19",
+    "url": "/portfolio-react/static/media/work-tgm-wire-mob-01.ee88173a.jpg"
+  },
+  {
+    "revision": "1949a54a8e11e518e6cf0aa952ea4b68",
+    "url": "/portfolio-react/static/media/work-tgm-wire-mob-02.1949a54a.jpg"
+  },
+  {
+    "revision": "7c1b142161881872b28ab0cdbe06842b",
+    "url": "/portfolio-react/static/media/work-tgm-wire-mob-03.7c1b1421.jpg"
+  },
+  {
+    "revision": "2a16e0f323d6a1760ef539b67945d4a2",
+    "url": "/portfolio-react/static/media/work-tgm-wire-mob-04.2a16e0f3.jpg"
+  },
+  {
+    "revision": "dd70f2ac290b5b92deb831a738dc4d9a",
+    "url": "/portfolio-react/static/media/work-viator-android.dd70f2ac.png"
+  },
+  {
+    "revision": "809853738f8c37f21fb2bf491c1d135a",
+    "url": "/portfolio-react/static/media/work-viator-designs.80985373.jpg"
+  },
+  {
+    "revision": "c5ec2f1f22291e3b79d8a0e8b5421a1c",
+    "url": "/portfolio-react/static/media/work-viator-mobile.c5ec2f1f.png"
+  },
+  {
+    "revision": "b1bae460f81e749f966359b408cfca17",
+    "url": "/portfolio-react/static/media/work-viator-sitemap.b1bae460.png"
+  },
+  {
+    "revision": "7671c56a6f51d2fe34545772e2ccdd69",
+    "url": "/portfolio-react/static/media/work-viator-wireframes.7671c56a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27864bd50631bbb3b5ba56e2e760dd14.js"
+  "/portfolio-react/precache-manifest.ac848772620fc147b0a0217112e3b8a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
