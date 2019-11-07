9d62bff31ec550db5be6315f24938188acee3947
jamieferguson <jamie@shorthand.com>
2019-11-07 15:24:20 +1100

    Stuff

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import BodyClassName from 'react-body-classname';
+import React from "react";
+import { Route } from "react-router-dom";
+import BodyClassName from "react-body-classname";
 
-import Nav from './Components/Nav';
-import WorkList from './Components/WorkList';
-import Page from './Components/Page';
-import Footer from './Components/Footer';
+import Nav from "./Components/Nav";
+import WorkList from "./Components/WorkList";
+import Page from "./Components/Page";
+import Footer from "./Components/Footer";
 
 const ScrollToTop = () => {
-	window.scrollTo(0, 0);
-	return null;
+  window.scrollTo(0, 0);
+  return null;
 };
 
 const Home = () => (
-	<BodyClassName className="home">
-		<Page>
-			<Route component={ScrollToTop} />
-			<div className="home">
-				<Nav />
-				<header>
-					<div className="centered">
-						<h1>Jamie Ferguson</h1>
-						<h2>
-							Product & Interaction Designer <span className="location">/ Sydney, Australia</span>
-						</h2>
-					</div>
-				</header>
-				<section id="work">
-					<h5>My work</h5>
-					<WorkList />
-				</section>
-				<Footer />
-			</div>
-		</Page>
-	</BodyClassName>
+  <BodyClassName className="home">
+    <Page>
+      <Route component={ScrollToTop} />
+      <div className="home">
+        <Nav />
+        <header>
+          <div className="Header--inner">
+            <h1>Jamie Ferguson</h1>
+            <h2>
+              Product & Interaction Designer <span className="location">/ Sydney, Australia</span>
+            </h2>
+          </div>
+        </header>
+        <section id="work">
+          <WorkList />
+        </section>
+        <Footer />
+      </div>
+    </Page>
+  </BodyClassName>
 );
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import './Styles/index.scss';
+import "./Styles/index.scss";
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-import Home from './Home';
-import WorkShorthand from './Work/WorkShorthand';
-import WorkViator from './Work/WorkViator';
-import WorkTGM from './Work/WorkTGM';
-import WorkAus from './Work/WorkAus';
+import Home from "./Home";
+import WorkShorthand from "./Work/WorkShorthand";
+import WorkViator from "./Work/WorkViator";
+import WorkTGM from "./Work/WorkTGM";
+import WorkAus from "./Work/WorkAus";
 
-const supportsHistory = 'pushState' in window.history;
+const supportsHistory = "pushState" in window.history;
 
 const Portfolio = () => (
-	<BrowserRouter forceRefresh={!supportsHistory}>
-		<main>
-			<Route
-				render={({ location }) => {
-					const { pathname } = location;
-					return (
-						<TransitionGroup>
-							<CSSTransition
-								key={pathname}
-								classNames={'page'}
-								timeout={{
-									enter: 0,
-									exit: 0
-								}}
-							>
-								<Route
-									location={location}
-									render={() => (
-										<Switch>
-											<Route exact path="/" component={Home} />
-											<Route path="/shorthand" component={WorkShorthand} />
-											<Route path="/viator" component={WorkViator} />
-											<Route path="/the-global-mail" component={WorkTGM} />
-											<Route path="/the-australian" component={WorkAus} />
-										</Switch>
-									)}
-								/>
-							</CSSTransition>
-						</TransitionGroup>
-					);
-				}}
-			/>
-		</main>
-	</BrowserRouter>
+  <BrowserRouter forceRefresh={!supportsHistory}>
+    <main>
+      <Route
+        render={({ location }) => {
+          const { pathname } = location;
+          return (
+            <TransitionGroup>
+              <CSSTransition
+                key={pathname}
+                classNames={"page"}
+                timeout={{
+                  enter: 0,
+                  exit: 0
+                }}
+              >
+                <Route
+                  location={location}
+                  render={() => (
+                    <Switch>
+                      <Route exact path="/" component={Home} />
+                      <Route path="/shorthand" component={WorkShorthand} />
+                      <Route path="/viator" component={WorkViator} />
+                      <Route path="/the-global-mail" component={WorkTGM} />
+                      <Route path="/the-australian" component={WorkAus} />
+                    </Switch>
+                  )}
+                />
+              </CSSTransition>
+            </TransitionGroup>
+          );
+        }}
+      />
+    </main>
+  </BrowserRouter>
 );
 
-render(<Portfolio />, document.getElementById('portfolio'));
+render(<Portfolio />, document.getElementById("portfolio"));
