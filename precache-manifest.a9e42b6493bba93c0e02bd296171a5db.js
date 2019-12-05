e500fd7487d05c9ffbaa9814f802cc4422b832c1
jamieferguson <jamie@shorthand.com>
2019-12-05 16:45:53 +1100

    Updates

---
+++ precache-manifest.a9e42b6493bba93c0e02bd296171a5db.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef714c3ffc80b12a1cd26087ef179af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a36f79a93ae213c2afbd",
+    "url": "/static/css/main.1a4851e8.chunk.css"
+  },
+  {
+    "revision": "53e302d37627dcff851a",
+    "url": "/static/js/2.66c4ed5e.chunk.js"
+  },
+  {
+    "revision": "a36f79a93ae213c2afbd",
+    "url": "/static/js/main.d0d8502a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60641adfcf3694f864c86896c05d67c4",
+    "url": "/static/media/index-aus.60641adf.jpg"
+  },
+  {
+    "revision": "2adc84a87b6e71b62049e5ccb8dd0138",
+    "url": "/static/media/index-shorthand.2adc84a8.jpg"
+  },
+  {
+    "revision": "68aab70cb62b21009e2fa5f346fc6956",
+    "url": "/static/media/index-tgm.68aab70c.jpg"
+  },
+  {
+    "revision": "923dd834e8d616b9a9df1440bf428f87",
+    "url": "/static/media/index-viator.923dd834.jpg"
+  },
+  {
+    "revision": "8febd423388d5bf84924d37e53bb5d6c",
+    "url": "/static/media/work-aus-election-01.8febd423.jpg"
+  },
+  {
+    "revision": "7e7617a9e776c304192453e5098e331f",
+    "url": "/static/media/work-aus-election-02.7e7617a9.jpg"
+  },
+  {
+    "revision": "9efda987e94fa2a9b1344f7ec8756199",
+    "url": "/static/media/work-aus-electionapp-01.9efda987.jpg"
+  },
+  {
+    "revision": "89245020e2c93441c7833c7cc9e29a38",
+    "url": "/static/media/work-aus-electionapp-02.89245020.jpg"
+  },
+  {
+    "revision": "6b2db49a4c9f49a15987d73cf317bd4d",
+    "url": "/static/media/work-aus-electionapp-03.6b2db49a.jpg"
+  },
+  {
+    "revision": "f7290b68718e9bf5d293f8a247213fc1",
+    "url": "/static/media/work-aus-electionapp-04.f7290b68.jpg"
+  },
+  {
+    "revision": "aeaf4453d686900ab4bceaba7ba544ae",
+    "url": "/static/media/work-aus-electionapp-05.aeaf4453.jpg"
+  },
+  {
+    "revision": "2538468095c2da6a2da3f3378474299b",
+    "url": "/static/media/work-aus-electionapp-06.25384680.jpg"
+  },
+  {
+    "revision": "d488c9510844388505b635616e5ad975",
+    "url": "/static/media/work-aus-yourschool-01.d488c951.jpg"
+  },
+  {
+    "revision": "19ebff897830eb2e5f1f65e61d5b01e0",
+    "url": "/static/media/work-aus-yourschool-02.19ebff89.jpg"
+  },
+  {
+    "revision": "d829c54fd5be5816083712ee0b761918",
+    "url": "/static/media/work-aus-yourschool-02a.d829c54f.jpg"
+  },
+  {
+    "revision": "7db0687c9856ebf27d1aad834f41e6f6",
+    "url": "/static/media/work-aus-yourschool-04.7db0687c.jpg"
+  },
+  {
+    "revision": "5941d52f2d7cd5647976c6d43f5ff038",
+    "url": "/static/media/work-aus-yourschool-05.5941d52f.jpg"
+  },
+  {
+    "revision": "173ffb274a89a7aa6e4e291faa40dd52",
+    "url": "/static/media/work-aus-yourschool-06.173ffb27.jpg"
+  },
+  {
+    "revision": "694dec738f7996bd3d91ed22ab1acc5b",
+    "url": "/static/media/work-sh-dash-01.694dec73.jpg"
+  },
+  {
+    "revision": "9793a5245e54b5d291a74af3b1f3a318",
+    "url": "/static/media/work-sh-dash-02.9793a524.jpg"
+  },
+  {
+    "revision": "fd3679ecb1a0d67b61af38e97cd9e7b8",
+    "url": "/static/media/work-sh-editor-01.fd3679ec.jpg"
+  },
+  {
+    "revision": "b09f83ac4bddb3bfb1d8702e6f23359e",
+    "url": "/static/media/work-sh-editor-02.b09f83ac.jpg"
+  },
+  {
+    "revision": "b6203c4b584f45c38bb9dfbc1e0e8aa8",
+    "url": "/static/media/work-tgm-bulga.b6203c4b.jpg"
+  },
+  {
+    "revision": "6c996d4d76baf909a5a90e3e12375069",
+    "url": "/static/media/work-tgm-partylines-01.6c996d4d.jpg"
+  },
+  {
+    "revision": "092a5667c6dd99db7b06edfd865da338",
+    "url": "/static/media/work-tgm-partylines-02.092a5667.jpg"
+  },
+  {
+    "revision": "60ded6b1df70bd5675db72bed4c8baef",
+    "url": "/static/media/work-tgm-pharma.60ded6b1.jpg"
+  },
+  {
+    "revision": "9556feaf0fb28710fc44f61395731504",
+    "url": "/static/media/work-tgm-politalk-01.9556feaf.jpg"
+  },
+  {
+    "revision": "f4c1937404db40010f226b95378fa0eb",
+    "url": "/static/media/work-tgm-politalk-02.f4c19374.jpg"
+  },
+  {
+    "revision": "25ff3f70ae8796e1fc628801ec1297d4",
+    "url": "/static/media/work-tgm-politalk-03.25ff3f70.jpg"
+  },
+  {
+    "revision": "74931a9ddcc60eb8132d557ef7648e53",
+    "url": "/static/media/work-tgm-politalk-04.74931a9d.jpg"
+  },
+  {
+    "revision": "5b2397f9d725e3bcda1297247f305ff4",
+    "url": "/static/media/work-tgm-rockart-01.5b2397f9.jpg"
+  },
+  {
+    "revision": "1cc22b07c458c5ab9ac50dd915fb6ea7",
+    "url": "/static/media/work-tgm-rockart-02.1cc22b07.jpg"
+  },
+  {
+    "revision": "284e867a463bc27627e85facdb39b7ac",
+    "url": "/static/media/work-tgm-wire-01.284e867a.jpg"
+  },
+  {
+    "revision": "7c9e73da5d13051ffbccb44df8500107",
+    "url": "/static/media/work-tgm-wire-02.7c9e73da.jpg"
+  },
+  {
+    "revision": "f000cf81a1ac9e231670111353acaa59",
+    "url": "/static/media/work-tgm-wire-03.f000cf81.jpg"
+  },
+  {
+    "revision": "293c5a978149b28e3f3a191cab7c0c6e",
+    "url": "/static/media/work-tgm-wire-04.293c5a97.jpg"
+  },
+  {
+    "revision": "ee88173a021581bc25db6b739dacca19",
+    "url": "/static/media/work-tgm-wire-mob-01.ee88173a.jpg"
+  },
+  {
+    "revision": "1949a54a8e11e518e6cf0aa952ea4b68",
+    "url": "/static/media/work-tgm-wire-mob-02.1949a54a.jpg"
+  },
+  {
+    "revision": "7c1b142161881872b28ab0cdbe06842b",
+    "url": "/static/media/work-tgm-wire-mob-03.7c1b1421.jpg"
+  },
+  {
+    "revision": "2a16e0f323d6a1760ef539b67945d4a2",
+    "url": "/static/media/work-tgm-wire-mob-04.2a16e0f3.jpg"
+  },
+  {
+    "revision": "dd70f2ac290b5b92deb831a738dc4d9a",
+    "url": "/static/media/work-viator-android.dd70f2ac.png"
+  },
+  {
+    "revision": "809853738f8c37f21fb2bf491c1d135a",
+    "url": "/static/media/work-viator-designs.80985373.jpg"
+  },
+  {
+    "revision": "c5ec2f1f22291e3b79d8a0e8b5421a1c",
+    "url": "/static/media/work-viator-mobile.c5ec2f1f.png"
+  },
+  {
+    "revision": "b1bae460f81e749f966359b408cfca17",
+    "url": "/static/media/work-viator-sitemap.b1bae460.png"
+  },
+  {
+    "revision": "7671c56a6f51d2fe34545772e2ccdd69",
+    "url": "/static/media/work-viator-wireframes.7671c56a.png"
+  }
+]);
